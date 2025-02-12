c6fc8c7972421f22fbb6c0b1ee59d76048019c7c
Siffaty Hassan <hassanmughal65700@gmail.com>
2025-02-12 17:52:23 +0500

    commit3

--- public/script.js
+++ public/script.js
@@ -1,18 +1,25 @@
-const swiper = new Swiper('.slider-wrapper', {
-    
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-   
-    
-  });
+let btn3 = document.getElementsByClassName("btn3")[0]; // First btn3 element
+let btn2 = document.getElementsByClassName("btn2")[0]; // First btn2 element
+let mainSlider = document.getElementsByClassName("main-slider")[0]; // First main-slider element
+let counter = 0;
+
+function slider() {
+  mainSlider.style.transform = `translateX(${counter * 33}rem)`; // Move the slider
+  if(counter>=4||counter<=-4){
+    return;
+  }
+}
+
+btn2.addEventListener('click', () => {
+  // console.log("btn2 is clicked");
+  counter--; // Example: Decrement the counter
+  console.log(counter)
+  slider(); // Update the slider position
+});
+
+btn3.addEventListener('click', () => {
+  // console.log("btn3 is clicked");
+  counter++; // Example: Increment the counter
+  console.log(counter)
+  slider(); // Update the slider position
+});
